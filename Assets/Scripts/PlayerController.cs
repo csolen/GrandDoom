1ca4f52e562e8741549767aa87a26847458c2941
Can <cansolen>
2025-11-18 16:45:11 +0100

    basic player movements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody2D rb;
+
+    public float moveSpeed = 5f;
+
+    private Vector2 moveInput;
+    private Vector2 mouseInput;
+
+    public float mouseSensitivity = 1f;
+
+    public Transform viewCam;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Vector3 moveHorizontal = transform.up * -moveInput.x;
+
+        Vector3 moveVertical = transform.right * moveInput.y;
+
+        rb.linearVelocity = (moveHorizontal + moveVertical) * moveSpeed;
+
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - mouseInput.x);
+
+        viewCam.localRotation = Quaternion.Euler(viewCam.localRotation.eulerAngles + new Vector3(0f, mouseInput.y, 0f));
     }
 }
