a68d9247554bc9e0454489dc34782d41ca8b6e44
Can <cansolen>
2025-11-19 00:36:08 +0100

    improvements for game editor script

--- Assets/Editor/GameEditor.cs
+++ Assets/Editor/GameEditor.cs
@@ -1,9 +1,13 @@
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameEditor
+public class GameEditor : EditorWindow
 {
+    private int sliderValue = 1;
+    private string levelName = "Level_1";
+
     [MenuItem("DOOM/Restart Scene")]
     private static void RestartScene()
     {
@@ -23,7 +27,7 @@ public class GameEditor
     [MenuItem("DOOM/Player/Immortal Mode")]
     private static void MakePlayerImmortal()
     {
-        
+
     }
 
     [MenuItem("DOOM/Player/Unlimited Ammo")]
@@ -44,4 +48,54 @@ public class GameEditor
 
     }
 
+    [MenuItem("DOOM/Load Level")]
+    public static void OpenWindow()
+    {
+        EditorWindow.GetWindow<GameEditor>("Level Loader");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Load Level", EditorStyles.boldLabel);
+        GUILayout.Space(5);
+
+        int newSliderValue = EditorGUILayout.IntSlider("Level Number:", sliderValue, 1, 100);
+
+        if (newSliderValue != sliderValue)
+        {
+            sliderValue = newSliderValue;
+            levelName = $"Level_{sliderValue}";
+        }
+
+        GUILayout.Space(10);
+
+        EditorGUILayout.LabelField("Level Name:", levelName);
+
+        GUILayout.Space(15);
+
+        if (GUILayout.Button("Load Scene"))
+        {
+            if (!EditorApplication.isPlaying)
+            {
+                SceneLoader(false);
+            }
+            else
+            {
+                SceneLoader(true);
+            }
+        }
+
+    }
+
+    private void SceneLoader(bool isEditorWorking)
+    {
+        if (isEditorWorking)
+        {
+            SceneManager.LoadScene(levelName);
+        }
+        else
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/" + levelName + ".unity");
+        }
+    }
 }
