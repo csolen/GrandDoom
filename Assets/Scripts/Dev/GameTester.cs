0f55b87f55444617b29e9c3eaf077bf2f3626277
Can <cansolen>
2025-11-25 00:49:19 +0100

    add gun tiles

--- Assets/Scripts/Dev/GameTester.cs
+++ Assets/Scripts/Dev/GameTester.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using TMPro;
 
 public class GameTester : MonoBehaviour
 {
@@ -61,7 +60,7 @@ public class GameTester : MonoBehaviour
             Cursor.visible = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             UnityEditor.EditorApplication.isPaused = true;
         }
