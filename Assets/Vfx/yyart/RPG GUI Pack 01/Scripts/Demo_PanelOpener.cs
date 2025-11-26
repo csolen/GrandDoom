8e6c911ced30bbbb661e2b8ed1142fc8991818b1
Can <cansolen>
2025-11-26 13:18:13 +0100

    add vfx package

---
+++ Assets/Vfx/yyart/RPG GUI Pack 01/Scripts/Demo_PanelOpener.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace yyart
+{
+    public class PanelOpener : MonoBehaviour
+    {
+        public GameObject Panel;
+
+        public void OpenPanel()
+        {
+            if(Panel !=null)
+            {
+                bool isActive = Panel.activeSelf;
+
+                Panel.SetActive(!isActive);
+            }
+        }
+    }
+}
