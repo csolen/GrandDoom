1f22b39ec5ec12e93ed2129255c4c518fc58a2e6
gorkem-hadi <gorkem@hadi.gs>
2025-11-27 17:06:27 +0300

    wall connector tiles added and enemy deathColliders fix

--- Assets/Scripts/Enemies/EnemyDestroyingAnimation.cs
+++ Assets/Scripts/Enemies/EnemyDestroyingAnimation.cs
@@ -3,7 +3,16 @@ using UnityEngine;
 public class EnemyDestroyingAnimation : MonoBehaviour
 {
     public GameObject deathParticle;
+    public GameObject deathHolder;
 
+    private CircleCollider2D deathHolderCircleCol;
+    private BoxCollider2D deathHolderBoxCol;
+
+    private void Awake()
+    {
+        deathHolderCircleCol = deathHolder.GetComponent<CircleCollider2D>();
+        deathHolderBoxCol = deathHolder.GetComponent<BoxCollider2D>();
+    }
     public void OpenDeathParticle()
     {
         deathParticle.SetActive(true);
@@ -11,6 +20,19 @@ public class EnemyDestroyingAnimation : MonoBehaviour
 
     public void RemoveAnimationObject()
     {
-        Destroy(gameObject);
+        Destroy(deathHolder);
+    }
+    public void DisableComponents()
+    {
+        if (deathHolderCircleCol != null) 
+        {
+            deathHolderCircleCol.enabled = false;
+        }
+
+        if (deathHolderBoxCol != null)
+        {
+            deathHolderBoxCol.enabled = false;
+        }
+        
     }
 }
