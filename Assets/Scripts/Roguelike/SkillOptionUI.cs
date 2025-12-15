5effe632d23d35a8bd65e09b72228aca2107b22d
Can <cansolen>
2025-12-15 22:49:13 +0300

    add new skill selection panel

--- Assets/Scripts/Roguelike/SkillOptionUI.cs
+++ Assets/Scripts/Roguelike/SkillOptionUI.cs
@@ -3,17 +3,32 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+[Serializable]
+public struct RarityColors
+{
+    public SkillRarity rarity;
+    public Color backgroundColor;
+    public Color titleAreaColor;
+}
+
 public class SkillOptionUI : MonoBehaviour
 {
     public Image iconImage;
     public TMP_Text titleText;
     public TMP_Text descriptionText;
     public TMP_Text rarityText;
+
     public Image backgroundImage;
+    public Image titleAreaImage;
+
     public Button selectButton;
 
-    public Image[] starImages;
-    public Sprite fullStarSprite;
+    public Image[] cardLevelIcons;
+    public Sprite cardLevelSpriteFull;
+
+    [Header("Rarity Colors")]
+    public RarityColors[] rarityColors;
+    public Color cardLevelSpriteFullColor;
 
     SkillData skillData;
     Action<SkillData> onSelected;
@@ -27,13 +42,9 @@ public class SkillOptionUI : MonoBehaviour
         titleText.text = data.displayName;
         descriptionText.text = data.description;
 
-        Color rarityColor = GetColorForRarity(data.rarity);
         rarityText.text = data.rarity.ToString();
-        rarityText.color = rarityColor;
 
-        Color c = rarityColor;
-        c.a = 0.35f;
-        backgroundImage.color = c;
+        ApplyRarityColors(data.rarity);
 
         selectButton.onClick.RemoveAllListeners();
         selectButton.onClick.AddListener(OnClick);
@@ -41,59 +52,70 @@ public class SkillOptionUI : MonoBehaviour
         SetupStars(data, currentLevel);
     }
 
+    void ApplyRarityColors(SkillRarity rarity)
+    {
+        foreach (var rc in rarityColors)
+        {
+            if (rc.rarity != rarity)
+                continue;
+
+            Color bg = rc.backgroundColor;
+            backgroundImage.color = bg;
+
+            if (titleAreaImage != null)
+                titleAreaImage.color = rc.titleAreaColor;
+
+            return;
+        }
+
+        // fallback
+        backgroundImage.color = Color.white;
+        if (titleAreaImage != null)
+            titleAreaImage.color = Color.white;
+    }
+
     void SetupStars(SkillData data, int currentLevel)
     {
-        if (string.IsNullOrEmpty(data.levelGroupId))
+        if (string.IsNullOrEmpty(data.levelGroupId) || currentLevel <= 0)
         {
-            for (int i = 0; i < starImages.Length; i++)
-                starImages[i].gameObject.SetActive(false);
-
+            SetAllStars(false);
             return;
         }
 
-        int maxStars = Mathf.Clamp(data.maxLevel, 1, starImages.Length);
-
-        if (currentLevel <= 0)
-        {
-            for (int i = 0; i < starImages.Length; i++)
-                starImages[i].gameObject.SetActive(false);
-
-            return;
-        }
-
+        int maxStars = Mathf.Clamp(data.maxLevel, 1, cardLevelIcons.Length);
         int clampedLevel = Mathf.Clamp(currentLevel, 0, maxStars);
 
-        for (int i = 0; i < starImages.Length; i++)
+        for (int i = 0; i < cardLevelIcons.Length; i++)
         {
             if (i >= maxStars)
             {
-                starImages[i].gameObject.SetActive(false);
+                cardLevelIcons[i].gameObject.SetActive(false);
                 continue;
             }
 
-            starImages[i].gameObject.SetActive(true);
+            cardLevelIcons[i].gameObject.SetActive(true);
 
             if (i < clampedLevel)
-                starImages[i].sprite = fullStarSprite;
+            {
+                cardLevelIcons[i].sprite = cardLevelSpriteFull;
+                cardLevelIcons[i].color = cardLevelSpriteFullColor;
+            }
+            else
+            {
+                cardLevelIcons[i].color = Color.white;
+            }
         }
     }
 
-    Color GetColorForRarity(SkillRarity rarity)
+    void SetAllStars(bool state)
     {
-        switch (rarity)
-        {
-            case SkillRarity.Common:
-                return Color.blue;
-            case SkillRarity.Rare:
-                return Color.green;
-            case SkillRarity.Legendary:
-                return Color.yellow;
-        }
-        return Color.white;
+        for (int i = 0; i < cardLevelIcons.Length; i++)
+            cardLevelIcons[i].gameObject.SetActive(state);
     }
 
     void OnClick()
     {
-        onSelected(skillData);
+        onSelected?.Invoke(skillData);
     }
 }
+
