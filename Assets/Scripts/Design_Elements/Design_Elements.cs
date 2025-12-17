d1c5e833ae9a54026768e69bf6fde26a1645173a
Can <cansolen>
2025-12-17 16:26:44 +0300

    add design elements

--- Assets/Scripts/Design_Elements/Design_Elements.cs
+++ Assets/Scripts/Design_Elements/Design_Elements.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class Design_Elements : MonoBehaviour
 {
     public SpriteRenderer sr;
+    private SpriteRenderer srBack;
 
     [SerializeField] private GameObject designElementHolder;
     [SerializeField] private Animator anim;
 
     public Sprite[] nextStageSr;
     private int hitCount = 0;
+
+    private void Awake()
+    {
+        srBack = GetComponent<SpriteRenderer>();
+        srBack.enabled = false;
+    }
 
     public void DesignElementGotHit()
     {
