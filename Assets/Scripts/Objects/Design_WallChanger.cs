def0a135eefcf2b29fd174ed506b4a7791dc2819
Can <cansolen>
2026-01-05 15:11:04 +0100

    change wall tile sprites

--- Assets/Scripts/Objects/Design_WallChanger.cs
+++ Assets/Scripts/Objects/Design_WallChanger.cs
@@ -4,39 +4,45 @@ public class Design_WallChanger : MonoBehaviour
 {
     private MeshRenderer mr;
 
-    public Material newMatNormal;
-    public Material newmatDarker;
+    public Material[] normalMats;
+    public Material[] darkMats;
 
     public GameObject destroyingParticle = null;
+
+    private bool alreadyChanged = false;
 
     private void Awake()
     {
         mr = GetComponent<MeshRenderer>();
 
         if (destroyingParticle != null)
-        {
             destroyingParticle.SetActive(false);
-        }
     }
 
     public void ChangeSprite()
     {
+        if (alreadyChanged)
+        {
+            return;
+        }
+
+        int index = normalMats.Length == 1
+            ? 0
+            : Random.Range(0, normalMats.Length);
+
         var mats = mr.materials;
 
-        if (mats.Length > 0 && mats[0] != newMatNormal)
-        {
-            mats[0] = newMatNormal;
-        }
+        if (mats.Length > 0)
+            mats[0] = normalMats[index];
 
-        if (mats.Length > 1 && mats[1] != newmatDarker)
-        {
-            mats[1] = newmatDarker;
-        }
+        if (mats.Length > 1)
+            mats[1] = darkMats[index];
 
         mr.materials = mats;
 
         if (destroyingParticle != null)
             destroyingParticle.SetActive(true);
-    }
 
+        alreadyChanged = true;
+    }
 }
